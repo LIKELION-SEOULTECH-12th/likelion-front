0d7f10f27befd34f2d4e47e65e295697b69d2471
doyeon <cat0806@naver.com>
2024-06-20 17:50:23 +0900

    8주차 세션 강의자료 - 개별 게시물 페이지 구현 및 api 연결

--- week5/src/App.js
+++ week5/src/App.js
@@ -6,6 +6,7 @@ import Signin from "./pages/Signin/Signin";
 import GlobalStyle from "./styles/GlobalStyle";
 import ProfileEdit from "./pages/Main/components/ProfileEdit";
 import ImagePost from "./pages/Upload/ImagePost";
+import Feed from "./pages/Feed/Feed";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/signin" element={<Signin />} />
         <Route path="/profileEdit" element={<ProfileEdit />} />
         <Route path="/imagePost" element={<ImagePost />} />
+        <Route path="/insta/:memberId/:postId" element={<Feed />} />
       </Routes>
     </div>
   );
