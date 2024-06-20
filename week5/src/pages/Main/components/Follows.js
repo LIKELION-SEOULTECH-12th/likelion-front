9e4e727a59f9aaad5ae94aa2accfd308f923fe4d
doyeon <cat0806@naver.com>
2024-06-20 17:49:43 +0900

    8주차 세션 강의자료 - 메인페이지 프로필 및 이미지 api 연결

--- week5/src/pages/Main/components/Follows.js
+++ week5/src/pages/Main/components/Follows.js
@@ -1,41 +1,38 @@
 import styled from "styled-components";
 
 const Follows = () => {
-    return ( 
+  return (
     <Followcontainer>
-        <Contentwrapper>
-            <span class="numbers">
-            <b>3</b>
-            posts
-            </span>
-            <span class="numbers">
-            <b>30</b>
-            followers
-            </span>
-            <span class="numbers">
-            <b>56</b>
-            following
-            </span>
-        </ Contentwrapper>
+      <Contentwrapper>
+        <span class="numbers">
+          <b>3</b>&nbsp; posts
+        </span>
+        <span class="numbers">
+          <b>30</b>&nbsp; followers
+        </span>
+        <span class="numbers">
+          <b>56</b>&nbsp; following
+        </span>
+      </Contentwrapper>
     </Followcontainer>
-    );
-}
+  );
+};
 export default Follows;
 
 const Followcontainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
 
 const Contentwrapper = styled.div`
-    display: flex;
-    justify-content: space-around;
-    padding: 20px;
-    align-items: center;
-    border-bottom: 1px solid gray;
+  display: flex;
+  justify-content: space-around;
+  padding: 20px;
+  align-items: center;
+  border-bottom: 1px solid gray;
 
-    @media all and (max-width: 767px) {
-        padding: 10px;
-    }
-`
+  @media all and (max-width: 767px) {
+    padding: 10px;
+  }
+`;
--- week5/src/pages/Main/components/ProfileContent.js
+++ week5/src/pages/Main/components/ProfileContent.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
@@ -7,10 +8,52 @@ const StyledLink = styled(Link)`
 `;
 
 const ProfileContent = () => {
+  const [profile, setProfile] = useState({
+    name: "",
+    nickname: "",
+  });
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const memberId = localStorage.getItem("id");
+
+    const fetchProfileInfo = async () => {
+      try {
+        const response = await fetch(
+          `http://127.0.0.1:8080/insta/user/${memberId}/info`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        setProfile({
+          name: data.name,
+          nickname: data.nickname,
+        });
+      } catch (error) {
+        console.error("프로필을 불러오는 데에 오류가 발생했습니다.", error);
+      } finally {
+        setLoading(false); // 성공 여부에 상관없이 로딩 상태 변경
+      }
+    };
+
+    fetchProfileInfo();
+  }, []);
+
   return (
     <Profilecontentcontainer>
-      <div className="nickname">
-        <h3>멋쟁이사자처럼</h3>
+      <div>
+        <div className="nickname">
+          <b>{loading ? "Loading..." : profile.nickname}</b>
+        </div>
+        <div className="name">{loading ? "Loading..." : profile.name}</div>
       </div>
       <div className="editBtn">
         <StyledLink to="/profileEdit">Edit Profile</StyledLink>
@@ -39,6 +82,13 @@ export default ProfileContent;
 const Profilecontentcontainer = styled.div`
   @media all and (max-width: 767px) {
     font-size: 80%;
+  }
+  .nickname {
+    font-size: 22px;
+  }
+  .name {
+    font-size: 20px;
+    margin-bottom: 10px;
   }
   .editBtn {
     width: 100px;
